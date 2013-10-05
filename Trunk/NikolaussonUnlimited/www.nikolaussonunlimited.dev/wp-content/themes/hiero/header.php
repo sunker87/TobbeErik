d447a798dc8d4b44647556e27d0e6791f9175586
sunker87 <erik.sundell87@gmail.com>
2013-10-05 16:14:01 +0200

    .

--- Trunk/NikolaussonUnlimited/www.nikolaussonunlimited.dev/wp-content/themes/hiero/header.php
+++ Trunk/NikolaussonUnlimited/www.nikolaussonunlimited.dev/wp-content/themes/hiero/header.php
@@ -43,8 +43,8 @@
 			<?php endif; ?>
                             
 			<nav id="main-navigation" class="main-navigation" role="navigation">
-<!--				<a href="#main-navigation" class="nav-open">Menu</a>
-				<a href="#" class="nav-close">Close</a>-->
+				<a href="#main-navigation" class="nav-open">Menu</a>
+				<a href="#" class="nav-close">Close</a>
 				<?php wp_nav_menu( array( 'container_class' => 'clearfix sf-menu', 'theme_location' => 'main' ) ); ?>
 			<!-- #main-navigation --></nav>
 		</div>
